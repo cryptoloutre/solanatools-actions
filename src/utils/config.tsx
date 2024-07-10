ddbba0d61aabf4be44719cfafb771ad88ecc2b1a
cryptoloutre <miningcryptomonnaie@gmail.com>
2024-07-10 20:05:51 +0200

    change to devnet

--- src/utils/config.tsx
+++ src/utils/config.tsx
@@ -1,7 +1,7 @@
-import { Keypair, PublicKey } from "@solana/web3.js"
+import { PublicKey } from "@solana/web3.js"
 
 const HELIUS_API_KEY = "194196fa-41b1-48f1-82dc-9b4d6ba2bb6c"
 
-export const RPC_URL = `https://mainnet.helius-rpc.com/?api-key=${HELIUS_API_KEY}`
+export const RPC_URL = `https://devnet.helius-rpc.com/?api-key=${HELIUS_API_KEY}`
 
 export const AUTHORITY = new PublicKey("authJFWStjt1LZmFh4kGVx83dWD8YpAXmm66bQGNsX1");

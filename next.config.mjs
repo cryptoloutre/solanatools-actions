8356513474b64bc56c321f9b0ef70342830f1e75
cryptoloutre <miningcryptomonnaie@gmail.com>
2024-07-19 14:39:29 +0200

    remove github page

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    basePath: "/solanatools-actions",
-    output: "export",
-    reactStrictMode: true,
-  };
-  
-  export default nextConfig;
+const nextConfig = {};
+
+export default nextConfig;
